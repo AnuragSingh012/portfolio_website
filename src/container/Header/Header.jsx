afd58f33365fc8676278e5fddf6ca1eb790604ab
Anurag Singh <anuragsingh4932@gmail.com>
2024-01-25 17:16:07 +0530

    Add Feature

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
           <h2 className='head-text'>I'm <span>Anurag Singh</span></h2>
           <h3 className='head-text'>Full Stack Developer</h3>
           <p>I'm a passionate full-stack web developer who loves turning ideas into interactive and user-friendly websites. With a knack for both front-end and back-end development. Let's collaborate and bring your digital vision to life!</p>
-          <button className='app__header-btn'>Download CV</button>
+          <button className='app__header-btn'><a href="https://drive.google.com/file/d/1h-bgLAjcZiFKHTlYHxfWYUzza3m4_1N5/view" target='_blank'>View Resume</a></button>
       </div>
       <div className='app__header-img'>
         <img src="" alt="profile_photo" />
